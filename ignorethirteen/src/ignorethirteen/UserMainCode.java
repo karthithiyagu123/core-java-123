8293be131f5ee24713ab0c9416504159beceec95
karthi <karthithiyagu9751@gmail.com>
2022-08-18 18:05:02 +0530

    first commit

---
+++ ignorethirteen/src/ignorethirteen/UserMainCode.java
@@ -0,0 +1,16 @@
+package ignorethirteen;
+
+public class UserMainCode {
+	public static int getLuckySum(int n1, int n2, int n3) {
+		if ((n1 == 13 && n2 == 13 && n3 == 13) || (n1 == 13 && n2 == 13) || (n1 == 13 && n3 == 13))
+			return 0;
+		else if (n1 == 13)
+			return n3;
+		else if ((n2 == 13) || (n2 == 13 && n3 == 13))
+			return n1;
+		else if (n3 == 13)
+			return n1 + n2;
+		else
+			return n1 + n2 + n3;
+	}
+}
